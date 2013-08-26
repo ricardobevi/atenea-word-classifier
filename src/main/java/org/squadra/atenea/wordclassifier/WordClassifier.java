373081cc44845a4897621df6f2b1fab39fa09047
facu <facu@localhost>
2013-08-26 03:29:40 -0300

    Clasificador de verbos segund Word de AteneaBase. Inclute modo Imperativo (poco testeado)

--- src/main/java/org/squadra/atenea/wordclassifier/WordClassifier.java
+++ src/main/java/org/squadra/atenea/wordclassifier/WordClassifier.java
@@ -2,14 +2,16 @@ package org.squadra.atenea.wordclassifier;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.squadra.atenea.wordclassifier.WordModel.Word;
+import org.squadra.atenea.base.word.Word;
+import org.squadra.atenea.base.word.WordTypes;
+
+import com.google.gson.Gson;
 
 /*
  * TODO:
@@ -22,7 +24,6 @@ import org.squadra.atenea.wordclassifier.WordModel.Word;
 
 public class WordClassifier {
 
-	private HashSet<String> classifiers;
 
 	// private Document doc;
 
@@ -32,28 +33,21 @@ public class WordClassifier {
 	 */
 	public static void main(String[] args) {
 
+		System.out.println("INICIANDO...");
+		
+		Gson gson = new Gson();
+		
 		WordClassifier WC = new WordClassifier();
 
-		Word result = WC.classifyWord("correr");
+		List<Word> results = WC.classifyWord("correr");
 
-		System.out.println(result);
-
-	}
-
-	public WordClassifier() {
-
-		classifiers = new HashSet<String>();
-
-		classifiers.add("adjetivo");
-		classifiers.add("verbo");
-		classifiers.add("interjección");
-		classifiers.add("nombre");
+		System.out.println("Resultados:");
+		//System.out.println(gson.toJson(results));
+		System.out.println(results.toString());
 
 	}
 
-	public Word classifyWord(String word) {
-
-		Word classifiedWord = new Word(word);
+	public List<Word> classifyWord(String word) {
 
 		boolean isVerb = false;
 
@@ -61,6 +55,8 @@ public class WordClassifier {
 		Elements content = new Elements();
 		String suggestedLink;
 
+		// Obtenemos la definicion de la RAE y si no coincide la palabra
+		// buscamos una segerencia
 		ArrayList<String> classifications = new ArrayList<String>();
 
 		try {
@@ -73,6 +69,8 @@ public class WordClassifier {
 
 			if (content.isEmpty()) {
 
+				// TODO: deberia traer varias sugerencias o ver si la que trae
+				// es igual
 				// No existe la palabra, se suguiere otra
 
 				suggestedLink = doc.getElementsByTag("a").get(0).attr("href");
@@ -89,6 +87,8 @@ public class WordClassifier {
 			e.printStackTrace();
 		}
 
+		// Clasificamos que tipo de palabra es
+
 		for (Element title : content) {
 
 			// Si ya se que es verbo no hace falta seguir preguntando
@@ -99,43 +99,75 @@ public class WordClassifier {
 			String[] spplitedTitle = title.attr("title").split(" ");
 
 			for (String titleWord : spplitedTitle) {
-				// Busco para encontrar el genero
 
-				if (classifiedWord.getGender().equals("") && 
-						(titleWord.equals("masculino") || titleWord.equals("fememino"))) {
+				/*
+				 * // Busco para encontrar el genero if
+				 * (classifiedWord.getGender().equals("") ) { if
+				 * (titleWord.equals("masculino") ) {
+				 * 
+				 * classifiedWord.setGender( WordTypes.Gender.MALE );
+				 * 
+				 * } else if( titleWord.equals("fememino") ) {
+				 * 
+				 * classifiedWord.setGender( WordTypes.Gender.FAMALE ); }
+				 * 
+				 * 
+				 * } else {
+				 * 
+				 * classifications.add(titleWord);
+				 * 
+				 * }
+				 */
 
-					classifiedWord.setGender(titleWord);
-
-				} else {
-
-					classifications.add(titleWord);
-
-				}
+				classifications.add(titleWord);
 
 			}
 
 		}
 
-		classifiedWord.setClassification(this
-				.searchClassifiers(classifications));
+		// Creo lista de Words para devolver
 
-		if (isVerb) {
-			System.out.println("----Es verbo:---- ");
-			classifiedWord.setConjugations(conjugate(doc));
+		ArrayList<Word> words = new ArrayList<Word>();
+
+		for (String classification : classifications) {
+
+			if (classification.equals("nombre")) {
+
+				words.add(new Word(word, WordTypes.Type.NOUN));
+
+			} else if (classification.equals("adjetivo")) {
+
+				words.add(new Word(word, WordTypes.Type.ADJECTIVE));
+
+			} else if (classification.equals("interjección")) {
+
+				words.add(new Word(word, WordTypes.Type.INTERJECTION));
+
+			} else if (classification.equals("verbo")) {
+
+				ArrayList<Word> conjugatedVerb = conjugate(doc, word);
+		
+				words.addAll(conjugatedVerb);
+			
+			}
+
 		}
 
-		return classifiedWord;
+		return words;
 
 	}
 
-	private HashMap<String, ArrayList<String>> conjugate(Document doc) {
+	/*************************************************************************/
+	/* obtengo las conjugaciones para un verbo */
+	
+	private ArrayList<Word> conjugate(Document doc, String baseWord) {
 
-		HashMap<String, ArrayList<String>> conjugations = new HashMap<String, ArrayList<String>>();
+		ArrayList<Word> conjugatedVerbs = new ArrayList<Word>();
 
 		String conjugationURL;
 		conjugationURL = getConjugationURL(doc);
 		String url = "http://lema.rae.es/drae/srv/" + conjugationURL;
-
+	
 		Document conjugationDoc = null;
 
 		try {
@@ -146,27 +178,78 @@ public class WordClassifier {
 			e.printStackTrace();
 		}
 
+		// Tomo modo indicativo y subjuntivo
+		
 		Elements blocks = conjugationDoc.getElementsByClass("x");
+		
+		for (int indexBlock = 0 ; indexBlock < blocks.size() ; indexBlock++) {	
 
-		for (Element block : blocks) {
-
+			Element block = blocks.get(indexBlock);
+			
 			String tense = block.getElementsByClass("y").html();
 
 			String rawConjugations = block.getElementsByClass("z").html();
 
-			ArrayList<String> conjugatedVerbs = new ArrayList<String>();
-
 			String[] splittedRawConjugations = rawConjugations.split("<br />");
 
 			for (Integer i = 0; i < splittedRawConjugations.length; i++) {
-				conjugatedVerbs.add(splittedRawConjugations[i]);
+					
+				String [] splittedConjugations =  splittedRawConjugations[i].split("/| o ");
+							
+				for( Integer j = 0;  j < splittedConjugations.length  ; j++ ){
+					
+					Word auxWord = new Word();
+					
+					auxWord.setTense( WordTypes.getTenseRaeClasification(tense)  );
+					
+					auxWord.setBaseWord(baseWord);
+					
+					auxWord.setPerson( WordTypes.getPersonByNumber(i) );
+					
+					auxWord.setType( WordTypes.Type.VERB  ); 
+					
+					auxWord.setName( splittedConjugations[j]);
+					
+					auxWord.setMode( ((indexBlock + 1) % 3) == 0 ?  WordTypes.Mode.SUBJUNCTIVE : WordTypes.Mode.INDICATIVE  );
+					
+					conjugatedVerbs.add(auxWord);
+				}
+				
 			}
 
-			conjugations.put(tense, conjugatedVerbs);
-
 		}
-
-		return conjugations;
+		
+		// Ahora tomo el modo imperativo
+		
+		Elements imperativeConjugationsBlock = conjugationDoc.getElementsByClass("r");
+		
+		Elements imperativeConjugationVerbs = imperativeConjugationsBlock.first().getElementsByClass("z");
+		
+		String imperativeConjugationVerb = imperativeConjugationVerbs.first().html();
+		
+		String[] splittedImperativeConjugationsVerbs = imperativeConjugationVerb.split("<br />|/");
+		
+		for( Integer k = 0; k < splittedImperativeConjugationsVerbs.length  ; k++ ){
+			
+			Word auxWord = new Word();
+			
+			auxWord.setBaseWord(baseWord);
+			
+			String[] auxImperativeVerb = splittedImperativeConjugationsVerbs[k].trim().split(" ");
+			
+			auxWord.setPerson( WordTypes.getPersonByImperativeRaeDefinition(auxImperativeVerb[1]) );
+			
+			auxWord.setType( WordTypes.Type.VERB  ); 
+			
+			auxWord.setName( auxImperativeVerb[0] );
+			
+			auxWord.setMode(  WordTypes.Mode.IMPERATIVE  );
+			
+			conjugatedVerbs.add(auxWord);
+		}
+		
+		
+		return conjugatedVerbs;
 
 	}
 
@@ -188,22 +271,6 @@ public class WordClassifier {
 		}
 
 		return conjugationURL;
-	}
-
-	private HashSet<String> searchClassifiers(ArrayList<String> classifications) {
-
-		HashSet<String> returnClassifiers = new HashSet<String>();
-
-		for (String classification : classifications) {
-
-			if (this.classifiers.contains(classification)) {
-				returnClassifiers.add(classification);
-			}
-
-		}
-
-		return returnClassifiers;
-
 	}
 
 }
